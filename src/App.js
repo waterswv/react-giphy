7cd9d8c77490dc1f2ddeb6d9bc18ddca65348020
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-07 12:01:50 -0700

    setup App and index.js to display hello world

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 // Developer TODO: Define App component defintion here
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default App
